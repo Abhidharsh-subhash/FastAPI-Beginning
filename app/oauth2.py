bd5d15a35c93e8f7f27e5b87047293a6e75d71aa
abhidharshss <abhidharsh1999@gmail.com>
2024-02-17 22:59:07 +0530

    all operations completed next is the foreign key relation model

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,9 +1,10 @@
 # jwt configurations
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 # essentially creating a reusable component that can be used to authenticate users by obtaining their access tokens
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -27,14 +28,15 @@ def verify_access_token(token: str, credentials_exception):
         if id is None:
             raise credentials_exception
         token_data = schemas.Tokendata(id=id)
-        print(token_data)
     except JWTError:
         raise credentials_exception
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail='could not validate credentials',
                                           headers={'WWW-Authenticate': 'Bearer'})
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter_by(id=token.id).first()
+    return user
--- app/routers/post.py
+++ app/routers/post.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix='/posts', tags=['Posts'])
 
 @router.get('/', response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db),
-              user_id: int = Depends(oauth2.get_current_user)):
+              current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""select * from posts""")
     # posts = cursor.fetchall()
     # the query object is performing the sql
@@ -18,29 +18,32 @@ def get_posts(db: Session = Depends(get_db),
     return posts
 
 
-@router.get('/{id}', response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
-    # x = find_post(id)
-    # if x is None:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-    #                         detail=f'Post with id:{id} not found')
-    #     # response.status_code = status.HTTP_404_NOT_FOUND
-    #     # return {'msg':f'Post with id:{id} not found'}
-    # else:
-    #     response.status_code = status.HTTP_200_OK
-    #     return {'data': x}
-    # cursor.execute("""select * from posts where id=%s""", (str(id)),)
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter_by(id=id).first()
-    db.query(models.Post).filter
-    return post
+# @router.get('/{id}', response_model=schemas.Post)
+# def get_post(id: int, db: Session = Depends(get_db)):
+#     # x = find_post(id)
+#     # if x is None:
+#     #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#     #                         detail=f'Post with id:{id} not found')
+#     #     # response.status_code = status.HTTP_404_NOT_FOUND
+#     #     # return {'msg':f'Post with id:{id} not found'}
+#     # else:
+#     #     response.status_code = status.HTTP_200_OK
+#     #     return {'data': x}
+#     # cursor.execute("""select * from posts where id=%s""", (str(id)),)
+#     # post = cursor.fetchone()
+#     post = db.query(models.Post).filter_by(id=id).first()
+#     return post
+@router.get('/user/', response_model=List[schemas.Post])
+def get_user_posts(db: Session = Depends(get_db),
+                   current_user: int = Depends(oauth2.get_current_user)):
+    user = db.query(models.Post).filter_by()
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 # the parameter will convert the body into dict format and assign it to the variable payload
 # def create_post(payload:dict = Body(...)):
 def create_post(new_post: schemas.PostCreate, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # here the new_post is a pydantic model
     # print(new_post)
     # pydantic model has a default method to convert the coming data into json format
@@ -60,7 +63,7 @@ def create_post(new_post: schemas.PostCreate, db: Session = Depends(get_db),
     # post = models.Post(title=new_post.title, content=new_post.content,
     #                    published=new_post.published)
     # it will unpack the new_post
-    print(user_id)
+    print(current_user)
     post = models.Post(**new_post.model_dump())
     db.add(post)
     db.commit()
@@ -73,7 +76,7 @@ def create_post(new_post: schemas.PostCreate, db: Session = Depends(get_db),
 # here the dependency is get_db
 @router.delete('/{id}')
 def delete_post(id: int, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""delete from posts where id=%s returning *""", (str(id)),)
     # post = cursor.fetchone()
     # conn.commit()
@@ -99,7 +102,7 @@ def delete_post(id: int, db: Session = Depends(get_db),
 
 @router.put('/{id}', response_model=schemas.Post)
 def update_post(id: int, new_post: schemas.PostCreate, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # x = find_post(id)
     # if x is None:
     #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
