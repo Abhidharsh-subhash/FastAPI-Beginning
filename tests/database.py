b91299f34a50b9c1d75eb0e805d32a42241610e3
abhidharshss <abhidharsh1999@gmail.com>
2024-03-01 16:32:43 +0530

    database error trying to resolve

--- tests/database.py
+++ tests/database.py
@@ -2,19 +2,12 @@ from fastapi.testclient import TestClient
 from app.main import app
 from app.config import settings
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import urllib.parse
 from app.database import Base
 from app.database import get_db
 import pytest
 
-# encoded_password = urllib.parse.quote_plus('abhidharsh@1999')
-# databasename = settings.database_name+'_test'
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{encoded_password}@{
-#     settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-# SQLALCHEMY_DATABASE_URL='postgresql://postgres:{encoded_password}@localhost:5432/fastapi_test'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
